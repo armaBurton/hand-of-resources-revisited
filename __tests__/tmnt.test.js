3bd6e190237d05385054af0abd080a8d00abc62e
armaBurton <armanarma@armaburton.com>
2022-06-16 14:05:26 -0700

    getAll() test passes

--- __tests__/tmnt.test.js
+++ __tests__/tmnt.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const TMNT = require('../lib/models/TMNT');
 
 describe('hand-of-resources-revisited routes', () => {
   beforeEach(() => {
@@ -10,5 +11,14 @@ describe('hand-of-resources-revisited routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should return a list of all dragons', async () => {
+    const expected = await TMNT.getAll();
+
+    const res = await request(app).get('/api/v1/tmnt');
+
+    console.log('|| res.body >', res.body);
+    expect(res.body).toEqual(expected);
   });
 });
